4695563af3dcca94ea44374fb55c3bb30bb97d7e
praveennick <praveennick1@gmail.com>
2021-04-11 16:30:29 +0530

    added about.js page

---
+++ src/containers/About.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+class About extends React.Component{
+    render() {
+        return (
+            <div>
+                
+  <section className="sub-header">
+    <nav>
+        <NavLink to="/"><img src="images/logo.png" alt="" /></NavLink>
+      <div className="nav-links" id="navLinks">
+        <i className="fa fa-times" onClick={()=>{ var navLinks = document.getElementById("navLinks");navLinks.style.right = "-200px";}} />
+        <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/course">Course</NavLink></li>
+          <li><NavLink to="/blog">Blog</NavLink></li>
+          <li><NavLink to="/contact">contact</NavLink></li>
+        </ul>
+      </div>
+      <i className="fa fa-bars" onClick={()=>{ var navLinks = document.getElementById("navLinks");navLinks.style.right = "0";}} />
+    </nav>
+    <h1>About Us</h1>
+  </section>
+  {/* -----------About Us Content=================== */}
+  <section className="about-us">
+    <div className="row">
+      <div className="about-col">
+        <h1>We are the World Largest University</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores molestias obcaecati veniam alias pariatur sapiente accusamus perferendis voluptates? Laborum vitae quos eligendi, ut dolores delectus odio magni molestias quasi facilis.</p>
+        <a href className="hero-btn red-btn">Explore Now</a>
+      </div>
+      <div className="about-col">
+        <img src="images/about.jpg" alt="" />
+      </div>
+    </div>
+  </section>
+
+            </div>
+        )
+    }
+}
+export default About;
