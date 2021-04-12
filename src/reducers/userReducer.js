162dc342adee222f8816c934d834396c5bc15951
praveennick <praveennick1@gmail.com>
2021-04-12 10:18:06 +0530

    added password check for login

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,8 +1,16 @@
 export default function(state=null,action){
     switch(action.type){
         case "LOGIN":
-            console.log("hello this is login");
-            return true;
+            console.log("hello this is login",action);
+            if (action.payload.data.result) {
+                return true;
+            }else{
+                alert("Incorrect password");    
+                localStorage.setItem("message",action.payload.data.message)
+                return false;
+                
+            }
+            
         case "LOGOUT":
             console.log("User Logged Out!");
             return false;
