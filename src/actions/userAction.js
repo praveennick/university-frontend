47f6f95fdc990919fbb00ce39ab93c085922e139
praveennick <praveennick1@gmail.com>
2021-05-01 11:52:11 +0530

    added register user and update user

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -9,6 +9,44 @@ export function loginUser(user){
         payload:promise
     }
 }
+export function registerNewUser(user) {
+    alert("Im in register action");
+    console.log("user", user);
+    var promise = axios.post("http://localhost:9545/register", user);
+    console.log("register-promise",promise)
+    return {
+      type: "REGISTER",
+      payload: promise,
+    };
+  }
+export function updateUser(user) {
+    alert("Im in update action");
+    
+    console.log("user", user);
+    var username=sessionStorage.getItem("name");
+      console.log("here is ths pid",username)
+    var promise = axios.put("http://localhost:9545/updateUser/"+username, user);
+    console.log("update-promise",promise);
+    if(user.s_name){
+        console.log("printed");
+        sessionStorage.setItem("name",user.s_name)
+    }
+    return {
+      type: "UPDATE",
+      payload: promise,
+    };
+  }
+
+export function displayUser(sid){
+    console.log("display action called")
+    // var sid=JSON.parse(localStorage.getItem("id"))
+    var promise = axios.get("http://localhost:9545/getUserById/"+sid);
+    console.log("promise",promise);
+    return{
+        type:"DISPLAY_USER",
+        payload:promise
+    }
+}
 
 export function logoutUser(){
     console.log("user loggedout");
